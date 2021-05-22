69767d227a816d470ff39ce77b71c84bc5dd3570
Foysal Ahammed <foysal.developer@gmail.com>
2021-05-23 00:53:52 +0600

    Projects sections design

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,17 +1,49 @@
+import styled from "@emotion/styled"
 import { DescriptionText, SectionTitle } from "../shared"
-
+const Styles = {
+  Wrapper: styled.section`
+    margin: 120px;
+    > ul {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      > li {
+        padding: 16px;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 30px;
+        text-align: center;
+        color: #211e2d;
+      }
+    }
+  `,
+  Flex: styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 24px;
+  `,
+  Grid: styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+  `,
+}
 const Projects = () => {
   return (
-    <>
-      <SectionTitle>Our Projects</SectionTitle>
-      <DescriptionText>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. A dictum eget
-        eleifend quam. Orci quis et cras viverra feugiat pretium. Maecenas
-        tortor, aliquet blandit ornare. Sem a mattis interdum consectetur.
-      </DescriptionText>
+    <Styles.Wrapper>
+      <SectionTitle center>Our Projects</SectionTitle>
+      <Styles.Flex>
+        <DescriptionText center>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. A dictum eget
+          eleifend quam. Orci quis et cras viverra feugiat pretium. Maecenas
+          tortor, aliquet blandit ornare. Sem a mattis interdum consectetur.
+        </DescriptionText>
+      </Styles.Flex>
       <ul>
         <li>
-          <a href='#'>All</a>
+          <a style={{ color: "#FF5300" }} href='#'>
+            All
+          </a>
         </li>
         <li>
           <a href='#'>Logo Design</a>
@@ -32,13 +64,15 @@ const Projects = () => {
           <a href='#'>Photo</a>
         </li>
       </ul>
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-      <img src='/project-image.PNG' alt='PROJECT TITLE ' />
-    </>
+      <Styles.Grid>
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+        <img src='/project-image.PNG' alt='PROJECT TITLE ' />
+      </Styles.Grid>
+    </Styles.Wrapper>
   )
 }
 
