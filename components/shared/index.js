cb8cdde59698388a187716121343e3bc532706d8
Foysal Ahammed <foysal.developer@gmail.com>
2021-05-23 21:10:50 +0600

    deleted unused components

--- components/shared/index.js
+++ components/shared/index.js
@@ -1,7 +1,5 @@
 import Button from "./Button"
 import SectionTitle from "./SectionTitle"
 import DescriptionText from "./DescriptionText"
-import Card from "./Card"
-import IconButton from "./IconButton"
 
-export { Button, SectionTitle, DescriptionText, Card, IconButton }
+export { Button, SectionTitle, DescriptionText }
